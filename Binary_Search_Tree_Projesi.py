2ec2c6d7020ee491f88de4ceb548490a5f4ae785
beyzayavuz06 <0beyza.yavuz0@gmail.com>
2025-07-06 16:49:02 +0300

    Binary_Search_Tree_Projesi

---
+++ Binary_Search_Tree_Projesi.py
@@ -0,0 +1,7 @@
+#[7, 5, 1, 8, 3, 6, 0, 9, 4, 2] dizisinin Binary-Search-Tree aşamalarını yazınız.
+#Root=7  
+#Root'un sağında 8 solunda 5
+#5'in sağında 6 , solunda 1
+#1’in solunda 0, sağında 3 bulunur.
+#3’ün solunda 2, sağında 4 bulunur.
+#8’in sağında 9 bulunur.
